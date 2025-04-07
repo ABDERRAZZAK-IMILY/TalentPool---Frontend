aacc837017bfcb96ad53829c1d9bfedd4c2aaaa8
abderrazzak imily <imily2024@gmail.com>
2025-04-07 11:32:16 +0100

    add nav bar to layout

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 export default function Layout({ children }) {
     return (
@@ -6,6 +6,20 @@ export default function Layout({ children }) {
             <header className="bg-blue-600 text-white p-4">
                 <h1 className="text-2xl">TALENTPOOL</h1>
             </header>
+
+            <nav className="bg-gray-800 text-white p-4">
+                <ul className="flex space-x-4">
+                    <li>
+    <Link to="/" className="hover:underline">Home</Link>
+                    </li>
+                    <li>
+<Link to={ "/login" } className="hover:underline">Login</Link>
+                        </li>
+                    <li>
+                         <Link to={ "/register" } className="hover:underline">Register</Link>
+                                            </li>
+                </ul>
+            </nav>
             <main className="flex-grow p-4">
                 <Outlet />
             </main>
