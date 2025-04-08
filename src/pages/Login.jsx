47122e8c5909738f0f17c36bbb2d42721d5c56ee
abderrazzak imily <imily2024@gmail.com>
2025-04-08 11:46:53 +0100

    add login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,7 +5,7 @@ import { axiosClient } from "../api/axios.js";
 
 export default function Login() {
 
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
 
     const [formData, setFormData] = useState({
         email: '',
@@ -55,12 +55,13 @@ export default function Login() {
         try {
             const response = await axiosClient.post('/login', formData);
             console.log(response.data);
-            navigate('/');
+       alert("tank you mester");
         } catch (error) {
             console.error(error);
             setErrors({ ...errors, server: 'Login failed. Please try again.' });
         }
     };
+
 
 
 
@@ -72,11 +73,11 @@ export default function Login() {
                 <form onSubmit={handleSubmit} >
                     <div className="mb-4">
                         <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
-                        <input type="email" id="email" className="mt-1 block w-full p-2 border border-gray-300 rounded" required />
+                        <input   value={formData.email}  type="email" id="email" className="mt-1 block w-full p-2 border border-gray-300 rounded" required />
                     </div>
                     <div className="mb-6">
                         <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
-                        <input type="password" id="password" className="mt-1 block w-full p-2 border border-gray-300 rounded" required />
+                        <input      value={formData.password}  type="password" id="password" className="mt-1 block w-full p-2 border border-gray-300 rounded" required />
                     </div>
                     <button type="submit" className="w-full bg-blue-500  py-2 rounded hover:bg-blue-600">Login</button>
                 </form>
